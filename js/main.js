e8f26a5dd139bdc4ab6bf4375f7f60a5a72dd6c9
Sarah Maris <sarah.87.maris@gmail.com>
2016-07-10 16:09:55 -0400

    Add different footer for each flag source

--- js/main.js
+++ js/main.js
@@ -1,16 +1,3 @@
-
-
-var njdotcomContest = '<a href="http://www.nj.com/entertainment/index.ssf/2016/02/' +
-                      'vote_for_a_new_flag_for_new_jersey.html" target="_blank">' +
-                      '<em>Vote for a new flage for New Jersey<em></a>' + 'posted by' +
-                      '<a href="http://connect.nj.com/user/Brian%20Donohue/posts.html" ' +
-                      'target="_blank"><strong>Brian Donohue</strong></a> on ' +
-                      '<a href="http://www.nj.com/ target="_blank">NJ.com</a>';
-
-var original = 'Designed based on feedback from the <a href="http://www.nj.com/entertainment/index.ssf/2016/02/' +
-                      'vote_for_a_new_flag_for_new_jersey.html" target="_blank">' +
-                      '<em>NJ.com flag design content<em></a>';
-
 var flagList = [
   {
     designer: "Nick Hawke and Andrew Maris",
@@ -19,8 +6,7 @@ var flagList = [
     description:"This flag uses the graphic story of the NJ militia uniform in " +
         " Andrew's original design and replaces with red and blue with the " +
         " traditional blue and buff New Jersey colors.",
-    showAll: false,
-    source: original
+    source: "original"
   },
   {
     designer: "Andrew Maris",
@@ -30,7 +16,7 @@ var flagList = [
         "uniform. The militia wore a blue coat over a red waistcoat, often " +
         "accompanied by a white sash. Three colors and star represent the third " +
         "state to join the union and the first to ratify the Bill of Rights.",
-    source: njdotcomContest
+    source: "njdotcomContest"
   },
   {
     designer: "Dan Shelffo",
@@ -40,7 +26,7 @@ var flagList = [
         "colors chosen by George Washington, symbolize beaches, ocean, sky and the " +
         "state's woodlands. The pine cone represents trees that thrive in the pine " +
         "barrens through sheer resiliency.",
-    source: njdotcomContest
+    source: "njdotcomContest"
   },
   {
     designer: "C. Erickson",
@@ -49,7 +35,7 @@ var flagList = [
     description: "The rays come from a central point, like a lighthouse on the " +
         "shore or the Statue of Liberty. It also evokes the invention of the light " +
         "bulb. Three rays symbolizes N.J.'s status as the third state in the union.",
-    source: njdotcomContest
+    source: "njdotcomContest"
   },
   {
     designer: "Marmocet",
@@ -58,7 +44,7 @@ var flagList = [
     description:"The cross shows N.J.'s heritage as an English colony. Buff and " +
         "blue are used on N.J.'s current flag. The shade of red was taken from " +
         "the cuffs and collars of NJ revolutionary war uniforms.",
-    source: njdotcomContest
+    source: "njdotcomContest"
   },
   {
     designer: "Alan Hall",
@@ -68,7 +54,7 @@ var flagList = [
         "outline of the state. The torch is a simplified version of the one held " +
         "by the Statue of Liberty. The three flames signify New Jersey's place " +
         "as the third state.",
-    source: njdotcomContest
+    source: "njdotcomContest"
   },
   {
     designer: "Andrew Zega",
@@ -77,7 +63,7 @@ var flagList = [
     description:"The band of buff represents the golden fields of the Garden State. " +
         "The blue is the sky and the seven-pointed white sun recalls the crown " +
         "of the Statue of Liberty",
-    source: njdotcomContest
+    source: "njdotcomContest"
   },
   {
     designer: "Andrew Jones",
@@ -85,7 +71,7 @@ var flagList = [
     image: "images/flag-7.jpg",
     description: "The cross reflects the flag of the isle of Jersey, from which  " +
         "New Jersey takes its name. The rest is from the current New Jersey flag.",
-    source: njdotcomContest
+    source: "njdotcomContest"
   },
   {
     designer: "Kenneth Huang",
@@ -93,7 +79,7 @@ var flagList = [
     image: "images/flag-8.jpg",
     description:"The yellow and blue harken back to the original state flag, " +
         "while the flame symbolizes knowledge, liberty, and power.",
-    source: njdotcomContest
+    source: "njdotcomContest"
   },
   {
     designer: "Dave Martucci",
@@ -103,7 +89,7 @@ var flagList = [
         "the design down to just the three plows on blue from the current arms. A " +
         "Revolutionary War N.J. flag reportedly just had the three plows and a " +
         "horse's head on it.",
-    source: njdotcomContest
+    source: "njdotcomContest"
   },
   {
     designer: "Joe Conklin",
@@ -111,7 +97,7 @@ var flagList = [
     image: "images/flag-10.jpg",
     description:"The traditional colors of New Jersey on a standard three-stripe " +
         "banner. Thirteen stars represent our original colonies.",
-    source: njdotcomContest
+    source: "njdotcomContest"
   },
   {
     designer: "",
@@ -119,11 +105,9 @@ var flagList = [
     image: "images/100-best.jpg",
     description:"",
     showLink: true,
-    source: njdotcomContest
+    source: "njdotcomContest"
   }
 ];
-
-
 
 function ViewModel(){
 
@@ -135,7 +119,9 @@ function ViewModel(){
   self.image = ko.observable();
   self.description = ko.observable();
   self.showLink = ko.observable(false);
-  
+  self.contest = ko.observable(false);
+  self.original = ko.observable(false);
+
   flagList.forEach( function( flag ){
     self.flags.push( flag );
   });
@@ -146,6 +132,8 @@ function ViewModel(){
     self.image( flag.image );
     self.description( flag.description );
     self.showLink(flag.showLink);
+    self.contest( flag.source === "njdotcomContest" );
+    self.original(  flag.source === "original" );
     $('#flag-modal').modal();
   };
 
